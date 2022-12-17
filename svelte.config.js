bc03b607b1831b37435e2da06ec9cae359922b40
Kayvan Arianpour <karianpour@gmail.com>
2022-12-17 19:42:43 +0300

    testing github pages config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = 'production' === 'development';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
