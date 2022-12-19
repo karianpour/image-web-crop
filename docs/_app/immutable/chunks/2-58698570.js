38ece71b326f96cefa088c3aa959d40f90d5108d
Kayvan Arianpour <karianpour@gmail.com>
2022-12-19 10:44:32 +0300

    build

---
+++ docs/_app/immutable/chunks/2-58698570.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-0f8beb86.js";export{t as component};
--- docs/_app/immutable/chunks/2-d52604a8.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-a90142c4.js";export{t as component};
